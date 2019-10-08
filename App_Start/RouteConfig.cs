73d4d8b0e68047f54736f171d3a4272a449026db
Andrew Chou <ac686115@ohio.edu>
2019-10-08 14:20:04 -0400

    SD1 done

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -14,9 +14,14 @@ namespace ac686115_MIS4200
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+              name: "Default",
+              url: "{controller}/{action}/{id}",
+              defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+              name: "Hello",
+              url: "{controller}/{action}/{name}/{id}"
             );
         }
     }
