1b35a106658ab9663ee67ff80319c389a7b5b610
Andrew Chou <ac686115@ohio.edu>
2019-10-08 15:38:57 -0400

    Need to fix patient.cs and doctor.cs

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ac686115_MIS4200.Models; // This is needed to access the models
+using System.Data.Entity; // this is needed to access the DbContext object
+
+namespace ac686115_MIS4200.DAL
+{
+    public class MIS4200Context : DbContext
+    {
+        public MIS4200Context() : base ("name=DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Doctor> Doctor { get; set; }
+        public DbSet<Patient> Patient { get; set; }
+    }
+}
---
+++ Models/Doctor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ac686115_MIS4200.Models
+{
+    public class Doctor
+    {
+        
+        [Key] 
+        public int doctorID { get; set; }
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string email { get; set; }
+
+        public string phone { get; set; }
+
+        public ICollection<Patient> Patient { get; set; }
+
+        public int patiendID { get; set; }
+
+       
+
+
+    }
+}
