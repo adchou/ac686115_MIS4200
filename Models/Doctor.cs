29d75dca1b772f7b26b9bfb1993a6e4cb1c4a36c
adchou <ac686115@ohio.edu>
2019-10-16 17:35:54 -0400

    Started SD3

--- Models/Doctor.cs
+++ Models/Doctor.cs
@@ -10,13 +10,29 @@ namespace ac686115_MIS4200.Models
     {
         
         [Key] 
+        [Display(Name = "Doctor ID")]
+        
+
         public int doctorID { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Doctors first name is required")]
+        [StringLength(50)]
         public string firstName { get; set; }
 
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Doctors last name is required")]
+        [StringLength(50)]
         public string lastName { get; set; }
 
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Enter your school email")]
+        [StringLength(50)]
         public string email { get; set; }
 
+        [Display(Name = "Phone number")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\(\d{3}\) |\d{3}-)\d{3}-\d{4}$", ErrorMessage ="Phone numbers must be in the format (xxx) xxx-xxxx or xxx-xxx-xxxx")]
         public string phone { get; set; }
 
         public int patientId {get; set; }
--- Models/Patient.cs
+++ Models/Patient.cs
@@ -9,16 +9,31 @@ namespace ac686115_MIS4200.Models
     public class Patient
     {
         [Key]
+        [Display(Name = "Patient ID")]
         public int patientID { get; set;}
 
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Patients first name is required")]
+        [StringLength(50)]
         public string firstName { get; set; }
 
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Patients first name is required")]
+        [StringLength(50)]
         public string lastName { get; set; }
 
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Enter your school email")]
+        [StringLength(50)]
         public string email { get; set; }
 
+        [Display(Name = "Phone number")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\(\d{3}\) |\d{3}-)\d{3}-\d{4}$", ErrorMessage = "Phone numbers must be in the format (xxx) xxx-xxxx or xxx-xxx-xxxx")]
         public string phone { get; set; }
-
+        [Display(Name = "When was your last visit?")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime patientSince { get; set; }
 
 
